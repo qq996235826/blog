8e37a2bede6a8d5af19582dc2e9a6196da7db5d6
邓末 <996235826@qq.com>
2020-11-26 03:24:12 +0800

    初始化完成

--- src/main/java/cn/deng/blog/controller/IndexController.java
+++ src/main/java/cn/deng/blog/controller/IndexController.java
@@ -1,7 +1,7 @@
 package cn.deng.blog.controller;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 
 /**
  * @author deng
@@ -15,7 +15,7 @@ public class IndexController {
      * 创建方法,用于索引
      * @return "index"
      */
-    @PostMapping("/")//一个反斜杠代表你啥都不输入的时候默认来这里
+    @GetMapping("/")//一个反斜杠代表你啥都不输入的时候默认来这里
     public String index()    //获得搜索框的内容,required=false表示该参数不是必要的
     {
         return "index";
